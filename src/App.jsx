0bf42e82cc8c1a181201a5be8f3d738411c6e97c
ajay-ackerman <ajayawchar8007@gmail.com>
2026-01-07 11:30:15 +0530

    added animated navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import Hero from './components/Hero'
 import About from './components/about'
+import Navbar from './components/Navbar'
+import { useGSAP } from '@gsap/react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <main className='relative min-h-screen w-screen overflow-x-hidden bg-violet-50'>
+      <Navbar />
       <Hero />
       <About />
     </main>
